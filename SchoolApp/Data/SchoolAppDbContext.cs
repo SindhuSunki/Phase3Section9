14d52a2bf4fed84ecf6812a84515e22734b76356
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-09-07 11:12:42 +0530

    first commit

---
+++ SchoolApp/Data/SchoolAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Model;
+
+namespace SchoolApp.Data
+{
+    public class SchoolAppDbContext : DbContext
+    {
+        public SchoolAppDbContext (DbContextOptions<SchoolAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SchoolApp.Model.RainBowSchool> RainBowSchool { get; set; } = default!;
+    }
+}
